a8d8dc9decdd3698490c82c3e77e3e52a1bf077f
Sam <eabird99@gmail.com>
2021-08-02 21:38:54 +1000

    07- TicTacToe..vue only use to do all function

--- 07-tictactoe-vuex/TdComponent.vue
+++ 07-tictactoe-vuex/TdComponent.vue
@@ -5,7 +5,7 @@
 </template>
 
 <script >
-  import {mapState} from './vuex';
+  import {mapState} from 'vuex';
   import {CLICK_CELL, NO_WINNER, RESET_GAME, SET_WINNER, CHANGE_TURN} from './store.js';
   // import EventBus from './EventBus.js';
 
@@ -34,50 +34,7 @@
       // },
     },
 
-    methods:{
-      onClickTd(){
-        // 이미 값이 있으면 중단하기.
-        if(this.cellData) return;
-        console.log('TdComponent start');
-        //this.$store.commit('CLICK_CELL'); // <- Vuex 방법으로  this.$set(this.tableData[rowIndex], cellIndex, this.turn);
-        //오타를 막기 위해 변수로 지정하여 사용.
-        this.$store.commit(CLICK_CELL,{row: this.rowIndex, cell: this.cellIndex});
-
-        let win = false;
-        	if(this.tableData[this.rowIndex][0] === this.turn &&this.tableData[this.rowIndex][1] === this.turn && this.tableData[this.rowIndex][2] === this.turn){
-          win = true;
-        }
-        	if(this.tableData[0][this.cellIndex] === this.turn && this.tableData[1][this.cellIndex] === this.turn && this.tableData[2][this.cellIndex] === this.turn){
-          win = true;
-        }
-        	if(this.tableData[0][0] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][2] === this.turn){
-          win = true;
-        }
-        	if(this.tableData[0][2] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][0] === this.turn){
-          win = true;
-        }
-        console.log('TdComponent check win ->' + win);
-        if(win){ 
-            this.$store.commit(SET_WINNER, this.turn);
-            this.$store.commit(RESET_GAME);
-        }else{
-          let all = true;
-          	this.tableData.forEach((row) => {
-            row.forEach((cell)=>{
-              if(!cell){
-                all=false;
-              }
-            })
-          });
-          console.log('TdComponent check all ->' + win);
-          if(all){
-            this.$store.commit(NO_WINNER); // <-this.$root.resetTable();
-            this.$store.commit(RESET_GAME);
-          }
-          	this.$store.commit(CHANGE_TURN);  //  <- this.turn = this.turn === 'O' ? 'X' : 'O';
-        }
-      }
-    }
+    
 
   };
 </script>
--- 07-tictactoe-vuex/TicTacToe.vue
+++ 07-tictactoe-vuex/TicTacToe.vue
@@ -1,27 +1,26 @@
 <template>
 	<div>
-		<table-component />
 		<div>{{turn}}'s turn</div>
-		<div v-show="win">
-			<h1>the winner is {{winner}}</h1>
-			<button v-on:click="resetTable">reStart</button>
-		</div>
+		<table>
+			<tr v-for="(rowData, rowIndex) in tableData" v-bind:key="rowIndex">
+				<td v-for="(cellData, cellIndex) in rowData" v-bind:key="cellIndex"  @click="onClickTd(rowIndex, cellIndex)">{{cellData}}</td>
+			</tr>
+		</table>
+		<div v-if="winner">the winner is {{winner}}</div>
 	</div>
 
 </template>
 
 <script>
 	import {mapState} from 'vuex';
-
-	import store from './store';
-	import TableComponent from './TableComponent.vue';
+	import store, {CLICK_CELL, NO_WINNER, RESET_GAME, SET_WINNER, CHANGE_TURN} from './store.js';
+	// import store from './store';
+	//import TableComponent from './TableComponent.vue';
 	//import EventBus from './EventBus.js';
 
 export default {
 	store,
-	components:{
-		TableComponent,
-	},
+
 	computed:{
 		...mapState(['winner', 'turn', 'tableData', 'win']),  // 아래 주석문을 짧게 가능.
 		// winner(){
@@ -65,8 +64,61 @@ export default {
 			this.winner = '';
 		},
 	},
+	methods:{
+      onClickTd(rowIndex, cellIndex){
+        // 이미 값이 있으면 중단하기.
+        if(this.cellData) return;
+        console.log('TdComponent start');
+        //this.$store.commit('CLICK_CELL'); // <- Vuex 방법으로  this.$set(this.tableData[rowIndex], cellIndex, this.turn);
+        //오타를 막기 위해 변수로 지정하여 사용.
+        this.$store.commit(CLICK_CELL,{row: rowIndex, cell: cellIndex});
+
+        let win = false;
+        	if(this.tableData[rowIndex][0] === this.turn &&this.tableData[rowIndex][1] === this.turn && this.tableData[rowIndex][2] === this.turn){
+          win = true;
+        }
+        	if(this.tableData[0][cellIndex] === this.turn && this.tableData[1][cellIndex] === this.turn && this.tableData[2][cellIndex] === this.turn){
+          win = true;
+        }
+        	if(this.tableData[0][0] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][2] === this.turn){
+          win = true;
+        }
+        	if(this.tableData[0][2] === this.turn && this.tableData[1][1] === this.turn && this.tableData[2][0] === this.turn){
+          win = true;
+        }
+        console.log('TdComponent check win ->' + win);
+        if(win){ 
+            this.$store.commit(SET_WINNER, this.turn);
+            this.$store.commit(RESET_GAME);
+        }else{
+          let all = true;
+          	this.tableData.forEach((row) => {
+            row.forEach((cell)=>{
+              if(!cell){
+                all=false;
+              }
+            })
+          });
+          console.log('TdComponent check all ->' + win);
+          if(all){
+            this.$store.commit(NO_WINNER); // <-this.$root.resetTable();
+            this.$store.commit(RESET_GAME);
+          }
+          	this.$store.commit(CHANGE_TURN);  //  <- this.turn = this.turn === 'O' ? 'X' : 'O';
+        }
+      }
+    }
 };
 </script>
 
 <style>
+  table{
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+  }
 </style>
